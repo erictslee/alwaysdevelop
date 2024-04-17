d9255b006b01b747355e03c6146b74e142a7f511
GaEun1216 <gani6382@gmail.com>
2024-04-17 16:53:50 +0900

    toggle 함수로 수정

--- delete.js
+++ delete.js
@@ -1,30 +1,25 @@
-
-// 모달을 여는 함수
-function openModal(a) { 
-    document.getElementById(a).style.display = 'block';
-}
-// 모달을 닫는 함수
-function closeModal(a) {
-    document.getElementById(a).style.display = 'none';
+// 모달 on off
+function eventModal(a){
+    $('#'+a).toggle();
 }
 
 // 비밀번호를 확인
 function CheckPW(){
     const pw = document.getElementById('pw').value;
     if(pw =="1234"){ // 임의로 1234로 지정 : firebase에서  uid로 비밀번호만 뽑아오기
-        openModal('modal');
+        eventModal('modal');
     }
     else{ // error 모달 띄우기
-        openModal('errormodal');
+        eventModal('errormodal');
     }
     document.getElementById('pw').value = ''; // 비밀번호 초기화
     // 해당 창 닫기
-    closeModal('enterpw');
+    eventModal('enterpw');
 }
 
 function confirmDelete() {
     console.log('firebase 지우는 함수')
-    closeModal('modal');
+    eventModal('modal');
 }
 
 function cancle(){
